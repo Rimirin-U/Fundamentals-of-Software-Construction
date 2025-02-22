0c57faa9394b9dcbfc6adada7c5050c18d2a71ae
Rimirin-U <T202410486994650@eduxiji.net>
2025-02-22 14:22:45 +0800

    initial

---
+++ assignment1/HWConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace HWConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            char op = Convert.ToChar(Console.Read());
+            double ans;
+            if (op == '-') ans = a - b;
+            else if (op == '*') ans = a * b;
+            else if (op == '/') ans = a / b;
+            else ans = a + b;
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ assignment1/HWWindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HWWindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            int opIndex = comboBox1.SelectedIndex;
+            double ans = 0;
+            if (opIndex == 0) ans = a + b;
+            else if (opIndex == 1) ans = a - b;
+            else if (opIndex == 2) ans = a * b;
+            else if (opIndex == 3) ans = a / b;
+            textBox3.Text = Convert.ToString(ans);
+        }
+    }
+}
