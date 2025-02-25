fca5d2b08333cfea818b9eee254415ee3066bd97
Rimirin-U <T202410486994650@eduxiji.net>
2025-02-25 22:08:36 +0800

    assignment2.3完成

---
+++ assignment2/HW3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace HW3
+{
+    class PrimeCalc
+    {
+        public int[] Calc(int n)
+        {
+            bool[] allNum = new bool[n + 1];//n+1个标识位，i为素数时allNum[i]==false
+            allNum[0] = true; allNum[1] = true;//0和1不是素数
+            for (int filter = 2; filter <= n / 2; filter++)
+            {
+                int notPrime = filter * 2;
+                while (notPrime <= n)
+                {
+                    allNum[notPrime] = true;//去掉filter的倍数
+                    notPrime += filter;
+                }
+            }
+            ArrayList primes = new ArrayList();
+            for (int i = 0; i <= n; i++)
+            {
+                if (!allNum[i]) primes.Add(i);
+            }
+            return (int[])primes.ToArray(typeof(int));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeCalc primeCalc = new PrimeCalc();
+            int[] primes = primeCalc.Calc(100);
+            int length = primes.Length;
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write($"{primes[i]} ");
+            }
+        }
+    }
+}
