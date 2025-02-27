b22dfa4a940c54af0387f965d54a8b6df85f4c17
Rimirin-U <T202410486994650@eduxiji.net>
2025-02-27 20:44:51 +0800

    assignment2.2修改（IO操作、异常检查）

--- assignment2/HW2/Program.cs
+++ assignment2/HW2/Program.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HW2
 {
+    //数组操作
     class ArrayCalc
     {
         public int Max(int[] arr)
@@ -40,39 +41,62 @@ namespace HW2
             return sum;
         }
     }
-    internal class Program
+    //IO
+    class MyIO
     {
-        static void Main(string[] args)
+        //输入整数数组
+        public int[] GetIntArray()
         {
+            //提示
+            Console.WriteLine("输入整数数组（回车分隔）");
             //偷用ArrayList
             ArrayList inList = new ArrayList();
-            ArrayCalc arrayCalc = new ArrayCalc();
             bool over = false;
             //循环读入
             while (!over)
             {
                 try
                 {
-                    int n = Convert.ToInt32(Console.ReadLine());
+                    string? instr = Console.ReadLine();
+                    //检查输入是否为空
+                    if (instr == "" || instr is null)
+                    {
+                        if (inList.Count == 0) throw new Exception("Illegal array: empty");//空数组
+                        else break;//输入完成
+                    }
+                    //转换；检查输入是否合法
+                    int n = Convert.ToInt32(instr);
                     inList.Add(n);
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
-                    break;
+                    Console.WriteLine(e.Message);
                 }
             }
             //转换为数组
             int[] arr = (int[])inList.ToArray(typeof(int));
+            return arr;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCalc arrayCalc = new ArrayCalc();
+            MyIO myIO = new MyIO();
+            int[] arr = myIO.GetIntArray();
+
             int max, min, sum;
             double mean;
             max = arrayCalc.Max(arr);
             min = arrayCalc.Min(arr);
             mean = arrayCalc.Mean(arr);
             sum = arrayCalc.Sum(arr);
-            Console.WriteLine(max);
-            Console.WriteLine(min);
-            Console.WriteLine(sum);
-            Console.WriteLine(mean);
+
+            Console.WriteLine($"max  = {max}");
+            Console.WriteLine($"min  = {min}");
+            Console.WriteLine($"sum  = {sum}");
+            Console.WriteLine($"mean = {mean}");
         }
     }
 }
