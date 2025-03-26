6bcfc060ce49d172007d0ed5ce19d3f23565a01b
Rimirin-U <T202410486994650@eduxiji.net>
2025-03-26 17:59:37 +0800

    test

---
+++ OrderManagementSystem/OrderServiceTest/OrderServiceUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OrderServiceTest
+{
+    [TestClass]
+    public class OrderServiceUnitTest
+    {
+        [TestMethod]
+        public void AddOrderTest1()
+        {
+            OrderService 
+        }
+    }
+}
