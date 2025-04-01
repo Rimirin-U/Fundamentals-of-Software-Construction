8d396a877b140aeb5f07a779c2881cff9246b37e
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-01 11:05:34 +0800

    assignment6 初始化

---
+++ OrderManagementSystem_WinForm/OrderManagementSystem_WinForm/Goods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem
+{
+    public class Goods
+    {
+        //Constructor
+        public Goods(string kind)
+        {
+            this.kind = kind;
+        }
+
+        //Properties
+        public string kind { get; set; }
+
+        //Functions
+        public override bool Equals(object? obj)
+        {
+            return obj is Goods goods &&
+                   kind == goods.kind;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(kind);
+        }
+        public override string ToString()
+        {
+            return $"Goods:\tKind = {kind}";
+        }
+    }
+}
---
+++ OrderManagementSystem_WinForm/OrderManagementSystem_WinForm/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem
+{
+    public class Order
+    {
+        //Constructor
+        public Order(OrderDetail orderDetail, int id)
+        {
+            this.orderDetail = orderDetail;
+            this.id = id;
+        }
+
+        //Properties
+        public OrderDetail orderDetail { get; set; }
+        public int id { get; }
+
+        //Functions
+        public override string ToString()
+        {
+            return $"Order:\n" + orderDetail;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is Order order &&
+                   EqualityComparer<OrderDetail>.Default.Equals(orderDetail, order.orderDetail) &&
+                   id == order.id;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(orderDetail, id);
+        }
+    }
+}
