8d35efd08f4ac2c4e6393025a01d9efbbdb01896
Rimirin-U <T202410486994650@eduxiji.net>
2025-04-08 19:46:35 +0800

    assignment8 接入数据库

---
+++ assignment8/OrderService/OrderDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext() : base("OrderDataBase")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<OrderDbContext>());
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
